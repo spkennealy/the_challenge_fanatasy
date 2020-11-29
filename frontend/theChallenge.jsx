eaeb3d1b439d5265dbcf8509947db844c84b6de1
Sean Kennealy <spkennealy@gmail.com>
2020-11-29 09:31:54 -0800

    theChallenge.jsx set up to render an h1 header for now

--- frontend/theChallenge.jsx
+++ frontend/theChallenge.jsx
@@ -1,32 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
-// import Root from './components/root';
-// var http = require("http");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const root = document.getElementById("root");
-
-//     // Bootstrap the user:
-//     let store;
-//     if (window.currentUser) {
-//         const preloadedState = {
-//             entities: {
-//                 users: { [window.currentUser.id]: window.currentUser }
-//             },
-//             session: { id: window.currentUser.id }
-//         };
-//         store = configureStore(preloadedState);
-
-//         delete window.currentUser;
-//     } else {
-//         store = configureStore();
-//     }
-//     // ------
-
-//     // ------------------- TESTS -------------------
-//     window.getState = store.getState;
-//     // ------------------- END -------------------
-
-//     ReactDOM.render(<Root store={store} />, root);
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>The Challenge</h1>, root);
+});
