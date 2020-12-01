f02dd2f72974241b33fdea78983b1ba89525f2ec
Sean Kennealy <spkennealy@gmail.com>
2020-12-01 08:33:01 -0800

    user.rb - password=(password) built

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,14 +31,19 @@ class User < ApplicationRecord
         SecureRandom.urlsafe_base64
     end
 
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
     def reset_session_token!
-        self.session_token = generate_session_token()
+        self.session_token = self.generate_session_token
         self.save!
         self.session_token
     end
 
     private
     def ensure_session_token
-        self.session_token ||= generate_session_token()
+        self.session_token ||= self.generate_session_token
     end
 end
