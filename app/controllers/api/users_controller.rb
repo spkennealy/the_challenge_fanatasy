ccebba7f8f31a6c444eac433668faaca96cd3ed6
Sean Kennealy <spkennealy@gmail.com>
2020-12-04 08:28:49 -0800

    users_controller.user_params added

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,4 +14,9 @@ class Api::UsersController < ApplicationController
             render json: @user.errors.messages, status: 422
         end
     end
+    
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
