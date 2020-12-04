203dee1068014bbd54126c2b0c8160e1db42178a
Sean Kennealy <spkennealy@gmail.com>
2020-12-04 08:28:02 -0800

    users_controller.create added

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,15 @@ class Api::UsersController < ApplicationController
         @user = User.new
         render :new
     end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render 'api/users/show.json.jbuilder'
+        else
+            render json: @user.errors.messages, status: 422
+        end
+    end
 end
