75481e239a7cdb5102813d07d1bda48ebfa83dfc
Sean Kennealy <spkennealy@gmail.com>
2020-12-05 11:14:25 -0800

    fixed webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: "bundle.js"
     },
+    // devServer: {
+    //     inline: false,
+    //     contentBase: __dirname + "/dist",
+    //     port: 3000
+    // },
     module: {
         rules: [
             {
@@ -14,7 +19,7 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
-                    query: {
+                    options: {
                         presets: ['@babel/env', '@babel/react']
                     }
                 },
