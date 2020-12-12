f76857e0b2f1c33e8dcebed9e21b7a654950319a
Sean Kennealy <spkennealy@gmail.com>
2020-12-12 10:08:21 -0800

    Added index & show for teams_controller

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,7 +1,13 @@
 class Api::TeamsController < ApplicationController
     def index
-        @teams = Team.find(params[:league_id])
+        @teams = current_user.teams
         render :index
     end
 
+    def show
+        @team = current_user.active_team
+        render :show
+    end
+
+    
 end
