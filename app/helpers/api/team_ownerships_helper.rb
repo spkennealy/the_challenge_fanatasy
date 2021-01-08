5c8d04e428606dd66ca176a49e79b965bcf20ed4
Sean Kennealy <spkennealy@gmail.com>
2021-01-07 16:48:18 -0800

    Created skeleton TeamOwnershipsController

---
+++ app/helpers/api/team_ownerships_helper.rb
@@ -0,0 +1,2 @@
+module Api::TeamOwnershipsHelper
+end
---
+++ test/controllers/api/team_ownerships_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::TeamOwnershipsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
