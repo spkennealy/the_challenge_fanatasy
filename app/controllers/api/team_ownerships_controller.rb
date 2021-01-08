cff983d09c43eed6e82c92790df95b9e00229455
Sean Kennealy <spkennealy@gmail.com>
2021-01-07 16:50:39 -0800

    TeamOwnershipsController.create method

--- app/controllers/api/team_ownerships_controller.rb
+++ app/controllers/api/team_ownerships_controller.rb
@@ -1,2 +1,11 @@
 class Api::TeamOwnershipsController < ApplicationController
+    def create
+        @team_ownership = TeamOwnership.new(team_ownership_params)
+    end
+
+    private
+
+    def team_ownership_params
+        params.require(:team_ownership).permit(:team_id, :user_id)
+    end
 end
