95e3238e19ebff2384f677d0dfcd9b65fe2c6093
Sean Kennealy <spkennealy@gmail.com>
2021-01-11 17:24:54 -0800

    TeamOwnershipsController.destroy

--- app/controllers/api/team_ownerships_controller.rb
+++ app/controllers/api/team_ownerships_controller.rb
@@ -3,6 +3,11 @@ class Api::TeamOwnershipsController < ApplicationController
         @team_ownership = TeamOwnership.new(team_ownership_params)
     end
 
+    def destroy
+        @team_ownership = TeamOwnership.find(params[:id])
+        @team_ownership.destroy
+    end
+
     private
 
     def team_ownership_params
