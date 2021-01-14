900d5bb952947b9ce74f9c64bbf5f93ce5574751
Sean Kennealy <spkennealy@gmail.com>
2021-01-14 13:03:37 -0800

    Add columns for team_challengers table

--- db/migrate/20210114210039_create_team_challengers.rb
+++ db/migrate/20210114210039_create_team_challengers.rb
@@ -1,8 +1,13 @@
 class CreateTeamChallengers < ActiveRecord::Migration[5.2]
   def change
     create_table :team_challengers do |t|
-
+      t.bigint :team_id, null: false
+      t.bigint :challenger_id, null: false
+      t.bigint :season_id, null: false
       t.timestamps
     end
+
+    add_index :team_challengers, :team_id
+    add_index :team_challengers, :challenger_id
   end
 end
