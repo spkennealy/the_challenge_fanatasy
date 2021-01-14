1911ed1769fe01d7c809d9e09a9537e31bc85068
Sean Kennealy <spkennealy@gmail.com>
2021-01-13 17:26:23 -0800

    LeagueManagersController.league_manager_params

--- app/controllers/api/league_managers_controller.rb
+++ app/controllers/api/league_managers_controller.rb
@@ -1,5 +1,11 @@
 class Api::LeagueManagersController < ApplicationController 
     def create
-        @league_manager = TeamOwnership.new()
+        @league_manager = TeamOwnership.new(league_manager_params)
+    end
+
+    private
+
+    def league_manager_params
+        params.require(:league_manager).permit(:league_id, :user_id)
     end
 end
