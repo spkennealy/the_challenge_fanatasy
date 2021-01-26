3296b8b1dee91f28abff9f20820075646c5ddc02
Sean Kennealy <spkennealy@gmail.com>
2021-01-25 17:43:42 -0800

    TeamsController.add_manager testing

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -13,12 +13,17 @@ class Api::TeamsController < ApplicationController
         @team = Team.new(team_params)
 
         if @team.save
+            add_manager()
             render :show
         else
             render json: @team.errors.full_messages, status: 422
         end
     end
 
+    def add_manager(team_id)
+        team_id = params[:id] if team_id == null
+        @team = Team.find(team_id)
+    end
 
     private 
 
