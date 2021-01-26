17a8a81a2c0fafe548ab4cb8fe9682f8aa701328
Sean Kennealy <spkennealy@gmail.com>
2021-01-25 17:32:09 -0800

    Removed LeagueManagersController.create

--- app/controllers/api/league_managers_controller.rb
+++ app/controllers/api/league_managers_controller.rb
@@ -1,16 +1,8 @@
 class Api::LeagueManagersController < ApplicationController 
-    def create
-        @league_manager = TeamOwnership.new(league_manager_params)
-
-        if @league_manager.save
-            render : 
-        else
-            render json: @league_manager.errors.full_messages, status: 422
-        end
-    end
+    
 
     def destroy
-        @league_manager = TeamOwnership.find(params[:id])
+        @league_manager = LeagueManager.find(params[:id])
         @league_manager.destroy
     end
 
