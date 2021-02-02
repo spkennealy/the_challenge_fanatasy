4eabaad9d2a786128f0a5a372487eaec8610ac13
Sean Kennealy <spkennealy@gmail.com>
2021-02-01 17:29:40 -0800

    Migrate league_teams

--- db/migrate/20210202012556_create_league_teams.rb
+++ db/migrate/20210202012556_create_league_teams.rb
@@ -1,8 +1,13 @@
 class CreateLeagueTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :league_teams do |t|
+      t.bigint :league_id, null: false
+      t.bigint :team_id, null: false
 
       t.timestamps
     end
+
+    add_index :league_teams, :team_id
+    add_index :league_teams, :league_id
   end
 end
