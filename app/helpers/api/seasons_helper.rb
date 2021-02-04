0a05c6ec1d571a1d61ea207c80ec5c2dfd528548
Sean Kennealy <spkennealy@gmail.com>
2021-02-03 20:35:37 -0800

    Moved all recently created controller files into api folders

---
+++ app/helpers/api/seasons_helper.rb
@@ -0,0 +1,2 @@
+module Api::SeasonsHelper
+end
--- test/controllers/api/seasons_controller_test.rb
+++ test/controllers/api/seasons_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SeasonsControllerTest < ActionDispatch::IntegrationTest
+class Api::SeasonsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
