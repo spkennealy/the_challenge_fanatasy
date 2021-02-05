61e3e8b7d8caa1005fada0aa5c75083c8c90487b
Sean Kennealy <spkennealy@gmail.com>
2021-02-05 08:34:25 -0800

    SeasonsController.show added

--- app/controllers/api/seasons_controller.rb
+++ app/controllers/api/seasons_controller.rb
@@ -2,4 +2,9 @@ class Api::SeasonsController < ApplicationController
     def index
         render :index
     end
+
+    def show
+        @season = Season.find(params[:id])
+        render :show
+    end
 end
